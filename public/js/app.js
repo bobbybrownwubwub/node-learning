cdadbe53ec10e72d4cb5606c943ae789416df3be
bobbybrownwubwub <bobbysupwork@gmail.com>
2020-04-21 12:04:13 +0200

    Setup for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,11 +6,13 @@ const input = document.querySelector("input");
 weather_form.addEventListener("submit", (e) => {
   e.preventDefault();
   let value = input.value;
-  console.log('test3')
+  console.log("test3");
   //const base = ""; //if 'http://localhost:3000'
   const url = `/weather?address=` + location;
   fetch(url).then((response) => {
+    console.log(response);
     response.json().then((data) => {
+      console.log(data);
       // if (data.message || data.features.length === 0) {
       //   error_msg = error || err_msg;
       //   error_msg = data.features.length === 0 ? "NO RESULTS" : error_msg;
